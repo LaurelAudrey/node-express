44026ae276526f54da53752a4e0fb376f299f79c
Laurel Elm <laurel.elm@gmail.com>
2023-01-14 10:19:48 -0700

    Add partnerRouter.js and update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const campsiteRouter = require('./routes/campsiteRouter');
+const partnerRouter = require('./routes/partnerRouter');
 const promotionRouter = require('./routes/promotionRouter');
 
 const hostname = 'localhost';
@@ -12,6 +13,7 @@ app.use(express.json());
 
 app.use('/campsites', campsiteRouter);
 app.use('/promotions', promotionRouter);
+app.use('/partners', partnerRouter);
 
 app.use(express.static(__dirname + '/public'));
 
